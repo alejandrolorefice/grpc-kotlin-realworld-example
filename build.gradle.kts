2c2f83b9b24e10d34170b73e03e44811749131cd
Alejandro Lorefice <alejandrolorefice1@gmail.com>
2023-10-15 18:17:16 +0200

    Add idea plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import com.google.protobuf.gradle.id
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    idea
+
     id("org.springframework.boot") version "3.1.4"
     id("io.spring.dependency-management") version "1.1.3"
     id("com.google.protobuf") version "0.9.4"
